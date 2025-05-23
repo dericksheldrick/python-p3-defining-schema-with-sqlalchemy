1690255897864bd4d025f86cf8fb326c894420a0
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-05-23 10:02:50 +0300

    assignment submission

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (Column, Integer, String, create_engine)
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key = True)
+    name= Column(String())
+
+    def __init__ (self, name):
+        self.name = name
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///lib/db/student.db')
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    Session = Session()
+    Session.commit()
